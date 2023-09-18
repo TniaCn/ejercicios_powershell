f59493e728a9d366ba8fdb54a69618dfa0ee76b0
TniaCn <alucan9252@ieselcaminas.org>
2023-09-18 09:42:36 +0200

    Practica2

---
+++ Practica2/Ej2.ps1
@@ -0,0 +1,4 @@
+﻿[int]$num1 = Read-Host "Escribe un número"
+[int]$num2 = Read-Host "Escribe otro número"
+$resultado=$num1+$num2
+Write-Host "La suma es $resultado"
---
+++ Practica2/Ej3.ps1
@@ -0,0 +1,4 @@
+﻿[int]$horas = Read-Host "Cuantas horas has trabajado?"
+[int]$coste = Read-Host "Cuanto cobras la hora?"
+$salario=$horas*$coste
+Write-Host "Tienes un salario de $salario euros"

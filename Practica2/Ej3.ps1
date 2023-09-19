b68a85bf56664ada8d06822221ac8eb65f7dbdad
TniaCn <alucan9252@ieselcaminas.org>
2023-09-19 12:46:00 +0200

    Práctica 2

--- Practica2/Ej3.ps1
+++ Practica2/Ej3.ps1
@@ -1,4 +1,4 @@
-﻿[int]$horas = Read-Host "Cuantas horas has trabajado?"
-[int]$coste = Read-Host "Cuanto cobras la hora?"
+﻿[double]$horas = Read-Host "Cuantas horas has trabajado?"
+[double]$coste = Read-Host "Cuanto cobras la hora?"
 $salario=$horas*$coste
 Write-Host "Tienes un salario de $salario euros"

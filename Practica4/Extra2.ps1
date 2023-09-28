636c0ed4ebb843a0d28ae3c9487572b7bde829ee
TniaCn <alucan9252@ieselcaminas.org>
2023-09-28 09:16:33 +0200

    Practica 4

---
+++ Practica4/Extra2.ps1
@@ -0,0 +1,13 @@
+﻿#triangulo con * y -
+[int]$num= Read-Host "Dime la altura del triangulo"
+for ($i=1; $i -le $num; $i=$i+1){
+    Write-Host ""
+    for ($j=1; $j -le $i; $j=$j+1){
+        if ($j -eq $i){
+            Write-Host -NoNewline "*"
+        }
+        else{
+            Write-Host -NoNewline "-"
+        }
+    }
+}

2a3c4e338230f5a968295731937f06757af37a67
TniaCn <alucan9252@ieselcaminas.org>
2023-09-28 08:17:24 +0200

    Practica 4

--- Practica4/Ej8.ps1
+++ Practica4/Ej8.ps1
@@ -1,4 +1,5 @@
-﻿[int]$num= Read-Host "Dime la altura del triangulo"
+﻿#triangulo con números impares
+[int]$num= Read-Host "Dime la altura del triangulo"
 for ($i=1; $i -le $num; $i=$i+1){
     Write-Host ""
     $escriure=$i*2-1

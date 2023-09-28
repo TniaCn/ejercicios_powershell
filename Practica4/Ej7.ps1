e9691fc3b7dfa61822fafaeae9ec901dbea1731e
TniaCn <alucan9252@ieselcaminas.org>
2023-09-28 08:16:18 +0200

    Practica 4

--- Practica4/Ej7.ps1
+++ Practica4/Ej7.ps1
@@ -5,3 +5,12 @@ for ($cont=1; $cont -le $num; $cont=$cont+1){
  Write-Host "$aster"
  $aster=$aster+"*"
 }
+
+#con dos bucles
+#[int]$num= Read-Host "a"
+#for ($i=1; $i -le $num; $i=$i+1){
+#   Write-Host ""
+#   for ($j=1; $j -le $i; $j=$j+1){
+#       Write-Host -NoNewline "*"
+#       }
+#}

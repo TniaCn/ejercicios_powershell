566002e5067e23d8a672dcb16ec5f96c23b17dd3
TniaCn <alucan9252@ieselcaminas.org>
2023-10-02 09:21:06 +0200

    Practica 5

---
+++ Practica5/Ej1.ps1
@@ -0,0 +1,55 @@
+﻿#Funciones Calculadora
+function suma ($num, $num2){
+$suma=$num + $num2
+Write-Host "La respuesta es $suma"
+}
+function resta ($num, $num2){
+$resta=$num - $num2
+Write-Host "La respuesta es $resta"
+}
+function multi ($num, $num2){
+$multi=$num * $num2
+Write-Host "La respuesta es $multi"
+}
+function div ($num, $num2){
+$div=$num / $num2
+Write-Host "La respuesta es $div"
+}
+
+#Programa principal
+
+do{
+[int]$calculadora=Read-Host "********** CALCULADORA **********
+1.Sumar
+2.Restar
+3.Multiplicar
+4.Dividir
+5.Salir
+¿Qué desea hacer? Elige una opción"
+if ($calculadora -ne 5){
+[int]$num=Read-Host "Dime un número"
+[int]$num2=Read-Host "Dime otro número"
+}
+Switch ($calculadora){
+    1 {
+      suma $num $num2
+      Read-Host
+      Clear-Host
+      }
+    2 {
+      resta $num $num2
+      Read-Host
+      Clear-Host
+      }
+    3 {
+      multi $num $num2
+      Read-Host
+      Clear-Host
+      }
+    4 {
+      div $num $num2
+      Read-Host
+      Clear-Host
+      }
+} 
+}while ( $calculadora -ne "5")
---
+++ Practica5/Ej2.ps1
@@ -0,0 +1,6 @@
+﻿#Lee los datos del archivo usuarios.csv
+$usuarios=Import-Csv Z:\Practica5\usuarios.csv
+foreach ($usu in $usuarios)
+{
+    Write-Host "$($usu.nombre), $($usu.apellidos), $($usu.grupo)"
+}

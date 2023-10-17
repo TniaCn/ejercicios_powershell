5a70965e47bdb2c33a99a5bd98f8023326ed4fb2
TniaCn <alucan9252@ieselcaminas.org>
2023-10-17 13:04:19 +0200

    AD

---
+++ AD/ADPermisos.ps1
@@ -0,0 +1,37 @@
+﻿#Crear estructura de carpetas
+
+
+#Carpeta principal
+New-Item -Path C:\ -Name Empresa -ItemType Directory
+
+#Subcarpetas
+$dep = Import-Csv C:\Users\Administrador\Documents\archivos\departamentos.csv -delimiter ";"
+foreach ($departamento in $dep)
+{
+    Write-Host $($departamento.departamento), $($departamento.descripcion)
+    New-Item -Path C:\Empresa -Name $($departamento.departamento) -ItemType Directory
+}
+
+
+
+#Dar permisos
+
+
+$dep = Import-Csv C:\Users\Administrador\Documents\archivos\departamentos.csv -delimiter ";"
+foreach ($departamento in $dep)
+{
+    Write-Host $($departamento.departamento), $($departamento.descripcion)
+    New-SmbShare -Path C:\Empresa\$($departamento.departamento) -Name $($departamento.departamento)
+}
+
+
+$dep = Import-Csv C:\Users\Administrador\Documents\archivos\departamentos.csv -delimiter ";"
+foreach ($departamento in $dep)
+{
+    Write-Host $($departamento.departamento), $($departamento.descripcion)
+    Grant-SmbShareAccess -Name $($departamento.departamento) -AccountName Administradores -AccessRight Full -Force
+    Grant-SmbShareAccess -Name $($departamento.departamento) -AccountName $($departamento.departamento) -AccessRight Change -Force 
+    Revoke-SmbShareAccess -Name $($departamento.departamento) -AccountName Todos -Force
+    Grant-SmbShareAccess -Name $($departamento.departamento) -AccountName "Usuarios del dominio" -AccessRight Read -Force 
+      
+}

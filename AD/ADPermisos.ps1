4308104e281f12e97a4e9f41f6d574855e72c07e
TniaCn <alucan9252@ieselcaminas.org>
2023-10-23 08:31:22 +0200

    scriptpermisosAD

--- AD/ADPermisos.ps1
+++ AD/ADPermisos.ps1
@@ -1,6 +1,5 @@
 ﻿#Crear estructura de carpetas
 
-<#
 
 #Carpeta principal
 New-Item -Path C:\ -Name Empresa -ItemType Directory
@@ -37,6 +36,27 @@ foreach ($departamento in $dep)
       
 }
 
-#>
 
-#ACLs
+#ACL
+
+$dep = Import-Csv C:\Users\Administrador\Documents\archivos\departamentos.csv -delimiter ";"
+foreach ($departamento in $dep)
+{
+    Write-Host $($departamento.departamento), $($departamento.descripcion)
+    $acl = Get-Acl -Path C:\Empresa\$($departamento.departamento)
+    $acl.SetAccessRuleProtection($true, $false)
+    $permisos = 'Administradores','FullControl','ContainerInherit,ObjectInherit','None','Allow'
+    $ace = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisos
+    $ace | Format-List
+    $permisos2 = "$($departamento.departamento)",'Modify','ContainerInherit,ObjectInherit','None','Allow'
+    $ace2 = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisos2
+    $ace2 | Format-List
+    $permisos3 = 'Usuarios del dominio','Read','ContainerInherit,ObjectInherit','None','Allow'
+    $ace3 = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisos3
+    $ace3 | Format-List
+    $acl.SetAccessRule($ace)
+    $acl.SetAccessRule($ace2)
+    $acl.SetAccessRule($ace3)
+    $acl | Set-Acl -Path C:\Empresa\$($departamento.departamento)
+}
+
